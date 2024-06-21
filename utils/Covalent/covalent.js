7b4a684ea87da16e826440c910bf5debb10d4e12
Dhrupad <thedhrupad@gmail.com>
2024-06-21 18:45:53 +0530

    fixed: wrote backend func

--- utils/Covalent/covalent.js
+++ utils/Covalent/covalent.js
@@ -1,3 +1,6 @@
+const {config} = require("dotenv")
+config();
+
 const  { CovalentClient } = require("@covalenthq/client-sdk");
 
 const client = new CovalentClient(`${process.env.COVALENT_API_KEY}`);
